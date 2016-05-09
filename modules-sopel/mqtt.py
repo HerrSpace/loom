9549a7cd775b7031fe5da0e064f91fd557b27b36
Patrick Meyer <git@the-space.agency>
2016-05-09 15:23:25 +0200

    Make mqtt bridge more usefull.

--- modules-sopel/mqtt.py
+++ modules-sopel/mqtt.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # ✓
 
@@ -6,21 +7,27 @@ mqtt.py - Sopel <-> MQTT bridge
 Copyright 2015, Patrick Meyer
 """
 
-MQ_HOST = "lux.z9"
+# TODO: Memory bridge
 
+import os
 import threading
 import re
 from sopel.module import rule, priority
 import mosquitto
 
+MQTT_BROKER = os.environ.get('MQTT_BROKER') or "localhost"
+
 def setup(sopel_instance):
     mq_conn = mosquitto.Mosquitto()
     mq_conn.on_message = on_mqtt_message
     mq_conn.on_disconnect = on_mqtt_disconnect
-    mq_conn.connect(host=MQ_HOST)
+    mq_conn.connect(host=MQTT_BROKER)
     sopel_instance.mq_conn = mq_conn
     mq_conn.subscribe(topic="msg/out/irc/+")
     mq_conn.user_data_set(sopel_instance)
+
+    if not sopel_instance.memory.contains("loom"):
+        sopel_instance.memory["loom"] = dict()
 
     threading.Thread(target=mq_conn.loop_forever).start()
 
@@ -32,21 +39,48 @@ def on_mqtt_disconnect(mosq, sopel, rc):
         setup(sopel)
 
 def on_mqtt_message(mosq, sopel, message):
-    m = re.search(r"msg/out/irc/(.*)", message.topic)
+    m = re.search(r"msg/out/+/irc/(.*)", message.topic)
     if not m: return
-    dest = unescape_source(m.groups()[0])
+    dest = unescape(m.groups()[0])
     msg = message.payload.decode('utf-8')
     
     sopel.say(msg, dest)
 
-@rule('.*')
+def _trigger_dict(trigger):
+    # more or less the sopel trigger:
+    # http://sopel.chat/docs/#the-trigger-class
+    # TODO: include uflags from bot.privileges[trigger.sender][bot.nick]
+    return {
+        "admin": trigger.admin,
+        "event": trigger.event,
+        "host": trigger.host,
+        "hostmask": trigger.hostmask,
+        "is_privmsg": trigger.is_privmsg
+        "nick": trigger.nick,
+        "owner": trigger.owner,
+        "raw": trigger.raw,
+        "sender": trigger.sender,
+        "tags": trigger.tags,
+    }
+
+@rule(r'!([^\s]+) (.*)')
 @priority('high')
 def on_irc_msg(bot, trigger):
-    source = escape_source(trigger.sender)
-    bot._bot.mq_conn.publish("msg/in/irc/"+source, trigger.group())
+    source = escape(trigger.sender)
+    bot._bot.mq_conn.publish("cmd/irc/"+source+"/", trigger.group())
+
+@rule(r'.*')
+@priority('high')
+def on_irc_msg(bot, trigger):
+
+    mq_msq = _trigger_dict(trigger)
+    mq_msq["msg"] = trigger.group()
+
+    # TODO: log message
+    bot._bot.mq_conn.publish("msg/in/irc/"+escape(trigger.sender), json.dumps(json_msg))
 
 # +#/ <- These are magic character in mqtt topics, so we need to work around them.
-def escape_source(source):
+def escape(source):
     return source.replace("#","😾").replace("+","☃").replace("/","🖕")
-def unescape_source(source):
+def unescape(source):
     return source.replace("😾","#").replace("☃","+").replace("🖕","/")
